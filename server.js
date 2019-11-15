92f65239c8ef8dcf6103e74e999d8ce0d7ee69e2
Dominick Maleszewski <53451678+maleszewskid@users.noreply.github.com>
2019-11-14 23:26:25 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,25 +5,20 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
 app.use('/public', express.static(path.join(__dirname + '/public')));
 
-// Parse application body
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-// Set Handlebars.
 const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access
 const routes = require("./controllers/burgers_controllers.js");
 
 app.use(routes);
 
-// Start the sever so that it can begin listening to client side requests
 app.listen(PORT, function() {
 
     console.log("Server listening on: http://localhost:" + PORT);
