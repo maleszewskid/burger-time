dd28fa442bbfba4773c68fc08f9e60d658d56605
Dominick Maleszewski <53451678+maleszewskid@users.noreply.github.com>
2019-11-15 00:04:41 -0500

    Update server.js

--- server.js
+++ server.js
@@ -19,8 +19,6 @@ const routes = require("./controllers/burgers_controllers.js");
 
 app.use(routes);
 
-app.listen(PORT, function() {
-
-    console.log("Server listening on: http://localhost:" + PORT);
-
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
