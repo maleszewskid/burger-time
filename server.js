c93889bb2ffc8e89716fb9993db06508b2ad3ca1
Dominick Maleszewski <53451678+maleszewskid@users.noreply.github.com>
2019-11-15 00:23:46 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
 const express = require('express');
 const path = require('path');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
+
 
 app.use('/public', express.static(path.join(__dirname + '/public')));
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+
 const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
+
 
 const routes = require("./controllers/burgers_controllers.js");
 
 app.use(routes);
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-    console.log("http://localhost:" + PORT);
+
+    console.log("Server listening on: http://localhost:" + PORT);
+
 });
