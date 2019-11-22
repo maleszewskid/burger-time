e9c488a34af6aec4685e476c2582961998618569
Dominick Maleszewski <53451678+maleszewskid@users.noreply.github.com>
2019-11-22 00:09:33 -0500

    Update burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,28 +1,22 @@
 var orm = require("../config/orm.js");
 
-
-//burger Model
-var burger = {
-    //display all
-    all: function(cb) {
-        orm.all("burgers", function(res) {
-            cb(res);
-        });
-    },
-    //create data 
-    create: function(cols, vals, cb) {
-        orm.update("burgers", cols, vals, function(res) {
-            cb(res)
-        })
-
-    },
-    //update ID to true
-    update: function(id, cb) {
-        var condition = "id=" + id;
-        orm.update("burgers", {
-            eaten: true
-        }, condition, cb);
-    }
+var burgers = {
+  all: function (cb) {
+    orm.all("burgers", function (res) {
+      cb(res);
+    });
+  },
+  create: function (name, cb) {
+    orm.create("burgers", ["burger_name", "eaten"],
+      [name, 0],
+      cb);
+  },
+  update: function (id, cb) {
+    var condition = "id=" + id;
+    orm.update("burgers", {eaten: true}, condition, cb);
+  }
 };
 
-module.exports = burger;
+
+
+module.exports = burgers;
