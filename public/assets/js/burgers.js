e4ea4d65fbdefced1cad93474147a263eafe7ede
Dominick Maleszewski <53451678+maleszewskid@users.noreply.github.com>
2019-11-21 22:10:05 -0500

    Update burgers.js

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -17,7 +17,47 @@ $(document).ready(function() {
             type: "PUT",
             data: 1
         }).then(
-            function() {
+            function() {$(document).ready(function () {
+
+  $(".submit").on("submit", function (event) {
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#add").val(),
+      devoured: 0
+    }
+
+    console.log(newBurger);
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(function (data) {
+   
+      location.reload();
+    });
+  });
+
+
+  $("#eat").on("click", function (event) {
+    event.preventDefault();
+
+    var id = $(this).data("id");
+
+    var newState = {
+      devoured: 1
+    }
+
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newState
+    }).then(
+      function () {
+
+        location.reload()
+      }
+    )
+  })
+});
                 location.reload()
             }
         )
