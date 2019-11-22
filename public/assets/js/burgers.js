95889a342e82418f96daccb913192548524cd004
Dominick Maleszewski <53451678+maleszewskid@users.noreply.github.com>
2019-11-22 00:11:27 -0500

    Update burgers.js

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,25 +1,38 @@
-$(document).ready(function() {
-    $(".submit").on("submit", function(event) {
-        console.log("clicked")
-        event.preventDefault();
-        var newBurger = $(this).children(".burger_id").val();
-        $.ajax({
-            method: "PUT",
-            url: "/burgers/" + newBurger
-        }).then(function(data) {
-            location.reload();
-        });
+$(document).ready(function () {
+
+  $(".submit").on("submit", function (event) {
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#add").val(),
+      eaten: 0
+    }
+
+    console.log(newBurger);
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(function (data) {
+
+      location.reload();
     });
-    $("#eat").on("click", function(event) {
-        event.preventDefault();
-        var burgerId = $(this).data("id")
-        $.ajax("/burgers/" + burgerId, {
-            type: "PUT",
-            data: 1
-        }).then(
-            function() {
-                location.reload()
-            }
-        )
-    })
-}); 
+  });
+
+  $("#eat").on("click", function (event) {
+    event.preventDefault();
+    var id = $(this).data("id");
+
+    var newState = {
+      eaten: 1
+    }
+
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newState
+    }).then(
+      function () {
+        location.reload()
+      }
+    )
+  })
+});
